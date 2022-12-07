44133a0ec1a6a3f5cf62050ee95eb49d82df3426
sufailbadushat <sufailbadushat@gamil.com>
2022-12-07 09:14:00 +0530

    add patient tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AddPatientComponent } from './add-patient/add-patient.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AddPatientComponent
   ],
   imports: [
     BrowserModule,
